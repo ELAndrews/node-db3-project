e68a63c10d5b1e64d569658bd50e7f1dd5a615ac
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-22 13:24:27 +0000

    Add SQL quries

--- queries.sql
+++ queries.sql
@@ -2,8 +2,34 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT p.ProductName, c.CategoryName
+FROM Product as p
+JOIN Category as c
+    on p.CategoryId = c.id
+ORDER BY c.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT o.id as Order_ID, s.CompanyName as Shipper_Company_Name
+FROM "Order" as o
+JOIN "Shipper" as s
+    on s.Id = o.ShipVia
+WHERE o.OrderDate < "2012-8-9"
+ORDER BY s.Id
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.ProductName, od.Quantity
+FROM OrderDetail as od
+JOIN Product as p
+    on p.Id = od.ProductId
+WHERE od.OrderId = "10251"
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.id as Order_ID, c.CompanyName, e.LastName as Employee_LastName
+FROM "Order" as o
+JOIN Customer as c
+    on o.CustomerId = c.Id
+JOIN Employee as e
+    on o.EmployeeId = e.Id
